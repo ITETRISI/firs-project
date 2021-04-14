d508870f03414dd0c8c3408d1ad9b53da353ebad
Daniil Osipenko <ocipenkoden@gmail.com>
2021-04-14 13:18:40 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListShellComponent } from './modules/users/containers/users-list-shell/users-list-shell.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersListShellComponent },
+  { path: '**',   redirectTo: 'users', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
